546bd35d33207b914cd6c3bbf507af9aafbb94a9
Jason Ray <jasonray@jayray.net>
2017-01-05 23:08:51 -0500

    add min test

--- test/numericlist-test.js
+++ test/numericlist-test.js
@@ -241,7 +241,7 @@ describe('numeric list', function () {
             list.push(1);
             list.min().should.be.equal(1);
         });
-        it ('given an all positive entry list with smallest at beginning, min should return the min', function () {
+        it('given an all positive entry list with smallest at beginning, min should return the min', function () {
             var list = new NumericList();
             list.push(13);
             list.push(22);
