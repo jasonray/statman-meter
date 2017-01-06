03958bd2a9f480a8319b0d874757a89319b1314c
Jason Ray <jasonray@jayray.net>
2017-01-05 20:06:06 -0500

    cleanup tests

--- test/numericlist-test.js
+++ test/numericlist-test.js
@@ -35,13 +35,13 @@ describe('numeric list', function () {
 
     it('given an empty list, sum should return 0', function () {
         var list = new NumericList();
-        assertCloseEnough(list.sum(), 0);
+        list.sum().should.equal(0);
     });
 
     it('given a list of one integer number, sum should return the number', function () {
         var list = new NumericList();
         list.push(5);
-        assertCloseEnough(list.sum(), 5);
+        list.sum().should.equal(5);
     });
 
     it('given a list of two integer numbers, sum should return the sum of the numbers', function () {
